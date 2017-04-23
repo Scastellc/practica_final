47f6c1512dfd63406f84c01b753a5b3a2a35b225
Salva <scastellc@gmail.com>
2017-04-23 13:47:50 +0200

    Creamos las paginas basicas

--- practica_final/tablero.php
+++ practica_final/tablero.php
@@ -2,77 +2,134 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 	<title>Ajedrez</title>
-	<link rel="stylesheet" href="stylo/estiloTablero.css">
-	<script src="js/jquery-1.7.2.min.js"></script>
-	<script src="http://code.jquery.com/ui/1.8.21/jquery-ui.min.js"></script>
-	<script src="js/jquery.ui.touch-punch.js"></script>
+		
 	
+	
+	<!-- 				Latest compiled and minified CSS 			-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+	<!--	 					Estilos								-->
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+		<link href="css/styloBasic.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+		<link href="css/estiloTablero.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+	<!-- 					Jquery						 			-->
+		<script src="js/jquery-1.7.2.min.js"></script>
+		<script src="http://code.jquery.com/ui/1.8.21/jquery-ui.min.js"></script>
+		<script src="js/jquery.ui.touch-punch.js"></script>
+
 </head>
 <body>
+ 	
+ 	<!--						Menu								-->
+	
+	<div class="container-fluid juego">	
+		<div class="row">
+			<nav class="navbar navbar-inverse navbar-fixed-top menu">
+				<div class="container-fluid">
+					<div class="navbar-header">
+			    		<button type="button" class="navbar-toggle btn glyphicon glyphicon-list btns" data-toggle="collapse" data-target="#MyMenu">					
+						</button>
+						<a class="navbar-brand" href="index.php" id="tit">Ajedrez Balear</a>
+					</div>
+					<div class="collapse navbar-collapse" id="MyMenu">	
+						<ul class="nav navbar-nav">
+							<li>
+								<a href="index.php">Inicio</a>
+							</li>
+							<li class="active">
+								<a href="tablero.php">Jugar</a>
+							</li>
+							<li>
+								<a href="noticias.php">Noticias</a>
+							</li>
+							<li>
+								<a href="" data-toggle="modal" data-target="#Cfun">¿Como funciona?</a>
+							</li>
+						</ul>
+						<div id=divDer class='nav navbar-nav navbar-right'>	
+							
+							<?php			
+								echo "Usuario registrado y eso...";
+							?>
+				    
+			    		</div>
+					</div>		
+		    	</div>
+		    </nav>
+	    </div>
+	</div>
 
-	<h1>Tablero</h1>
-	<div class="tablero">
-		<ul id="casillas">
+				<!-- 				Modal de carta							-->
+	<div class="modal fade" id="Cfun" role="dialog">
+	    <div class="modal-dialog">
+			<!-- 					Contenedor del modal 				-->
+	    	<div class="modal-content">
+	        	<div class="modal-header">
+	          		<button type="button" class="close" data-dismiss="modal">&times;</button>
+	          		<h3 class="modal-title">Funcionamiento</h3>
+	        	</div>
+	        	<div class="modal-body">
+	        		<div class="text-center">
+	        			        			
+			        	<?php 
+			        		echo "Como funciona y eso...";
+						?>
+	        		
+	        		</div>
+	        	</div>
+	        	<div class="modal-footer modal-right">
+		        <button type="button" class="btn" data-dismiss="modal">Cerrar</button>
+	        	</div>
+	      	</div>
+	    </div>
+	</div>
+	<div id="juego" class="container"> 
+		<div class="row">
+	   		<div class="col-lg-12">
+				<div class="col-md-10 col-md-offset-2">
+					<div class="tablero">
+						<h1>Tablero</h1>
+						<ul id="casillas">
 
-		<?php 
-			// Creamos un array para la anotacion de cada casilla
-			$letras = array(1 => "a", 2 => "b", 3 => "c", 4 => "d", 5 => "e", 6 => "f", 7 => "g", 8 => "h");
+						<?php 
+							// Creamos un array para la anotacion de cada casilla
+							$letras = array(1 => "a", 2 => "b", 3 => "c", 4 => "d", 5 => "e", 6 => "f", 7 => "g", 8 => "h");
 
-			// Hacemos dos for para crear el tablero de 8 x 8
-			for($a = 8; $a>=1; $a--){
-				for($i = 1; $i<=8; $i++){
-					if($a%2==0){
-						// Le agregamos la casilla con su identificador y la clase trash para arrastrar y soltar
-						if($i%2==0)
-						{
-							echo '<div class="blanco" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
-						}else{
-							echo '<div class="negro" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
-						}
-					}
-					else{					
-						if($i%2==0){
-							echo '<div class="negro" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
-						}else{
-							echo '<div class="blanco" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
-						}
-					}
-				}
-			}
-		?>
-		</ul><hr/>
+							// Hacemos dos for para crear el tablero de 8 x 8
+							for($a = 8; $a>=1; $a--){
+								for($i = 1; $i<=8; $i++){
+									if($a%2==0){
+										// Le agregamos la casilla con su identificador y la clase trash para arrastrar y soltar
+										if($i%2==0)
+										{
+											echo '<div class="blanco" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
+										}else{
+											echo '<div class="negro" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
+										}
+									}
+									else{					
+										if($i%2==0){
+											echo '<div class="negro" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
+										}else{
+											echo '<div class="blanco" id="c'.$letras[$i].$a.'"><span class="trash" id="'.$letras[$i].$a.'"></span></div>';
+										}
+									}
+								}
+							}
+						?>
+						</ul><hr/>
+					</div>
+				</div>		
+			</div>
+		</div>		
 	</div>
 	
     <script>
     
-	    $(function(){
-			// Creamos dos variables, una todo lo que esta dentro de las casillas, y otra que es la que puede soltar objetos
-	      var $casillas = $( "#casillas" ),
-	          $move = $( ".trash" );
-
-	      $( "li", $casillas ).draggable({
-	        helper: "clone",
-	        cursor: "move",
-	      });
-
-	      // Donde se va a soltar
-	      $move.droppable({
-			   	hoverClass: "dejarPieza",
-	        	drop: function( event, ui ){
-				
-				var id = $(this).attr("id");//recuperamos el id de donde se soltara la pieza
-				var idprovene = ui.draggable.attr("id");//cogemos el titilo de lo movido	
-				alert(id + " " + idprovene);		
-				ui.draggable.append().appendTo( "#"+id ).show();//colocamos la pieza
-				var nuevoid = $(this).prop("id", id);
-				var nuevoT = $(this).attr("title", id);
-				alert(nuevoid + " " + nuevoT);
-	        }
-	      }); 		   
-	    });
-
 		// Piezas blancas
 
 		$("#a1").html('<li title="a1" id="a1"><img src="piezas/torreB.png"/></li>');
@@ -116,8 +173,73 @@
 	    $("#f7").html('<li title="f7" id="f7"><img src="piezas/peonN.png"/></li>');
 		$("#g7").html('<li title="g7" id="g7"><img src="piezas/peonN.png"/></li>');
 		$("#h7").html('<li title="h7" id="h7"><img src="piezas/peonN.png"/></li>');
+		
+	    $(function(){
+			// Creamos dos variables, una todo lo que esta dentro de las casillas, y otra que es la que puede soltar objetos
+	      var $casillas = $( "#casillas" ),
+	          $move = $( ".trash" );
+
+	      $( "li", $casillas ).draggable({
+	        helper: "clone",
+	        cursor: "move",
+	      });
+
+	      // Donde se va a soltar
+	      $move.droppable({
+			   	hoverClass: "dejarPieza",
+	        	drop: function(event, ui ){
+				
+				var id = $(this).attr("id");//recuperamos el id de donde se soltara la pieza
+				var idprovene = ui.draggable.attr("id");//cogemos el titilo de lo movido	
+				alert(idprovene + " ->va a:-> " + id);		
+				
+				var nuevoid = $(this).prop("id", id);
+				var nuevoT = $(this).attr("title", id);
+				alert(nuevoid.parentNode + " " + nuevoT.parentNode);
+				ui.draggable.append().appendTo( "#"+id ).show();//colocamos la pieza
+	        }
+	      }); 		   
+	    });
+
 
     </script>
 
+		<!-- 			Con este div separamos el footer				-->
+	<div class="clearfix"></div>
+
+ 	<!--		Footer		-->
+	<div class="footer">
+	    <div class="container">
+	        <div class="row">
+	    		<div class="col-lg-12">
+	      			<div class="col-md-8">
+	      				<p> 
+	      					<?php
+	      						echo "Hola";
+							?>	
+						</p>
+	      				<a href="#">Terms of Service</a> | <a href="#">Privacy</a> 
+			        	<div class="nav navbar-nav navbar-right icons">
+							<a href="https://www.facebook.com" target="blank"><i class="fa fa-facebook-square fa-4x social social-fb"></i></a>
+	            			<a href="https://plus.google.com" target="blank"><i class="fa fa-google-plus-square fa-4x social social-gp"></i></a>
+	            			<a href="https://twitter.com" target="blank"><i class="fa fa-twitter-square fa-4x social social-tw"></i></a>
+						</div> 			   
+	      			</div>
+	      			<div class="col-md-4">
+	        			<p class="muted pull-right">© 2016 Salva Castell. All rights reserved</p>
+	      			</div>
+                </div>
+            </div>    
+        </div>
+    </div>
+
 </body>
+
+	<!--Bootstrap-->
+	<!-- 					jQuery library 							-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<!-- 				Latest compiled JavaScript 					-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </html>
