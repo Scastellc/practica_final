7dc573198a7d74da5b8e0458ea1e2d106c470061
Scastellc <Scastellc@gmail.com>
2017-05-29 23:31:16 +0200

    Anotacion

--- practica_final/js/funciones.js
+++ practica_final/js/funciones.js
@@ -1,3 +1,6 @@
+var _quienToca = "blancas";
+var _colorPieza="";
+
 $(document).ready(function(){
  	
  	//console.log($("#mostrarmodal"));
@@ -21,7 +24,7 @@ $(document).ready(function(){
 function tablero(){
 
 	// Creamos un array para la anotacion de cada casilla
-	//var letras = ["","a", "b", "c", "d", "e", "f", "g", "h"];
+	var letras = ["","a", "b", "c", "d", "e", "f", "g", "h"];
 
 		// Hacemos dos for para crear el tablero de 8 x 8
 		for(var y = 8; y >= 1; y--){
@@ -40,17 +43,44 @@ function tablero(){
 						color = "negro";
 					}
 				}
-				$("#casillas").append("<div class='" + color + "' id='" + x+""+y + "'>");
-				// <span class='trash' id='" + letras[i] + a + "'></span></div>
+				$("#casillas").append("<div data-pieza=\"\" data-color=\"\" class='" + color + "' id='" + x+""+y + "'><li title='"+letras[x]+""+y+"' id='"+x+""+y+"'></li>");
 			}
 		}
 }
 
 // Funcion para colocar las piezas en el tablero
 function piezas(){
+		
+		// Casillas
+		var letras = ["","a", "b", "c", "d", "e", "f", "g", "h"];
 
-		var blancas = ["Bpeones.png"];
+		// Imagenes de piezas blancas y negras
+		var blancas = ["Bpeones.png","Btorre.png","Bcaballo.png","Balfil.png","Bdama.png","Brey.png","Balfil.png","Bcaballo.png","Btorre.png"];
+		var negras = ["Npeon.png","Ntorre.png","Ncaballo.png","Nalfil.png","Ndama.png","Nrey.png","Nalfil.png","Ncaballo.png","Ntorre.png"];
 
+		for (var i = 1; i < 9; i++) {
+			$("#"+i+"1").html('<li title="'+letras[i]+1+'" id="'+letras[i]+1+'"><img src="piezas/'+$(blancas[i]).selector+'"/></li>');
+			//$("#"+letras[i]+"1").html('<img src="piezas/'+$(blancas[i]).selector+'"/>');
+			$("#"+i+"2").html('<li title="'+letras[i]+2+'" id="'+letras[i]+2+'"><img src="piezas/'+$(blancas[0]).selector+'"/></li>');
+
+			$("#"+i+"1").attr("data-pieza",blancas[i]);
+  			$("#"+i+"1").attr("data-color","Blancas");
+  			$("#"+i+"2").attr("data-pieza",blancas[0]);
+  			$("#"+i+"2").attr("data-color","Blancas");
+		}
+
+		for (var i = 9; i > 0; i--) {
+			$("#"+i+"8").html('<li title="'+letras[i]+8+'" id="'+letras[i]+8+'"><img src="piezas/'+$(negras[i]).selector+'"/></li>');
+			//$("#"+letras[i]+"1").html('<img src="piezas/'+$(blancas[i]).selector+'"/>');
+			$("#"+i+"7").html('<li title="'+letras[i]+7+'" id="'+letras[i]+7+'"><img src="piezas/'+$(negras[0]).selector+'"/></li>');
+
+			$("#"+i+"8").attr("data-pieza",negras[i]);
+  			$("#"+i+"8").attr("data-color","Negras");
+  			$("#"+i+"7").attr("data-pieza",negras[0]);
+  			$("#"+i+"7").attr("data-color","Negras");
+		}
+
+/*
   		// Piezas blancas
   		$("#11").html('<li title="a1" id="a1"><img src="piezas/Btorre.png"/></li>');
  		$("#21").html('<li title="b1" id="b1"><img src="piezas/Bcaballo.png"/></li>');
@@ -60,17 +90,11 @@ function piezas(){
  	    $("#61").html('<li title="f1" id="f1"><img src="piezas/Balfil.png"/></li>');
  		$("#71").html('<li title="g1" id="g1"><img src="piezas/Bcaballo.png"/></li>');
  		$("#81").html('<li title="h1" id="h1"><img src="piezas/Btorre.png"/></li>');
- 
- 		$("#44").html('<li title="d4" id="d4"><img src="piezas/Btorre.png"/></li>');
- 		$("#46").html('<li title="d4" id="d4"><img src="piezas/Balfil.png"/></li>');
- 		$("#45").html('<li title="d4" id="d4"><img src="piezas/Bcaballo.png"/></li>');
- 		$("#65").html('<li title="d4" id="d4"><img src="piezas/Bdama.png"/></li>');
- 		$("#25").html('<li title="d4" id="d4"><img src="piezas/Brey.png"/></li>');
-
-
+*/
  			// Peones blancos
- 
- 		$("#12").html('<li title="a2" id="a2"><img src="piezas/"'+blancas[0]+'"/></li>');
+ /*
+ 		$("#12").html('<li title="a2" id="a2"><img src="piezas/'+$(blancas[0]).selector+'"/></li>');
+ 		//$("#12").html('<li title="b2" id="b2"><img src="piezas/Bpeones.png"/></li>');
  		$("#22").html('<li title="b2" id="b2"><img src="piezas/Bpeones.png"/></li>');
  		$("#32").html('<li title="c2" id="c2"><img src="piezas/Bpeones.png"/></li>');
  		$("#42").html('<li title="d2" id="d2"><img src="piezas/Bpeones.png"/></li>');
@@ -78,9 +102,9 @@ function piezas(){
  	    $("#62").html('<li title="f2" id="f2"><img src="piezas/Bpeones.png"/></li>');
  		$("#72").html('<li title="g2" id="g2"><img src="piezas/Bpeones.png"/></li>');
  		$("#82").html('<li title="h2" id="h2"><img src="piezas/Bpeones.png"/></li>');
- 
+ */
  		// Piezas Negras
- 
+ /*
  		$("#18").html('<li title="a8" id="a8"><img src="piezas/Ntorre.png"/></li>');
  		$("#28").html('<li title="b8" id="b8"><img src="piezas/Ncaballo.png"/></li>');
  		$("#38").html('<li title="c8" id="c8"><img src="piezas/Nalfil.png"/></li>');
@@ -100,7 +124,7 @@ function piezas(){
     	$("#67").html('<li title="f7" id="f7"><img src="piezas/Npeon.png"/></li>');
  		$("#77").html('<li title="g7" id="g7"><img src="piezas/Npeon.png"/></li>');
  		$("#87").html('<li title="h7" id="h7"><img src="piezas/Npeon.png"/></li>');
- 
+ */
 }
 
 // Esta funcion marca o desmarca la pieza que has seleccionado
@@ -124,7 +148,10 @@ function pieza($event, liPieza){
 
 	// Recupero el nombre de la pieza
 	var img = $event.currentTarget.currentSrc;
-	var pieza = img.substr(63,1);
+	//var pieza = img.substr(78,1);
+	var pieza = img.substr(28,1);
+	console.log(img);
+	console.log(pieza);
 	//var pieza = img.substr(78,1);
 
 	// Cogo el id de donde esta la pieza (sin x,y) sino con la notacion normal de ajedrez
@@ -188,8 +215,8 @@ function pieza($event, liPieza){
 	function movePeon($event, x, y, xIni, yIni){		
 	
 		var img = $event.currentTarget.currentSrc;
-		//var pieza = img.substr(77,1);
-		var pieza = img.substr(62,1);
+		var pieza = img.substr(27,1);
+		//var pieza = img.substr(62,1);
 		switch(pieza){				
 			case "B":
 				movePeonB($event, x, y, xIni, yIni);		
@@ -218,7 +245,7 @@ function pieza($event, liPieza){
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("",num);
+				anotacionB("",num);
 				$("#"+p1).unbind("click");
 				$("#"+p2).unbind("click");
 			})
@@ -229,7 +256,7 @@ function pieza($event, liPieza){
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("",num);
+				anotacionB("",num);
 				$("#"+p1).unbind("click");
 				$("#"+p2).unbind("click");
 			})
@@ -243,7 +270,7 @@ function pieza($event, liPieza){
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("",num);
+				anotacionB("",num);
 				$("#"+p1).unbind("click");
 			})
 		}
@@ -266,7 +293,7 @@ function pieza($event, liPieza){
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("",num);
+				anotacionN("",num);
 				$("#"+p1).unbind("click");
 				$("#"+p2).unbind("click");
 			})
@@ -277,7 +304,7 @@ function pieza($event, liPieza){
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("",num);
+				anotacionN("",num);
 				$("#"+p1).unbind("click");
 				$("#"+p2).unbind("click");
 			})
@@ -291,7 +318,7 @@ function pieza($event, liPieza){
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("",num);
+				anotacionN("",num);
 				$("#"+p1).unbind("click");
 			})
 		}
@@ -302,6 +329,7 @@ function pieza($event, liPieza){
 
 		// Recuperamos la imagen
 		var img = $event.currentTarget.currentSrc;
+		var color = img.substr(27,1);
 
 		// Creamos un evento nuevo
 		var eventPieza = $event;
@@ -351,9 +379,18 @@ function pieza($event, liPieza){
 				// Colocamos el rey en su nueva casilla				
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				// Volvemos a llamar a la funcion para que desmarque todo
-				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("R",num);
+
+				if (color == "B") {
+					$("#"+$event.currentTarget.id).attr("data-pieza","Brey.png");  				
+					$("#"+$event.currentTarget.id).attr("data-color","Blancas");
+					anotacionB("R",num);
+				}else{
+					$("#"+$event.currentTarget.id).attr("data-pieza","Nrey.png"); 
+					$("#"+$event.currentTarget.id).attr("data-color","Negras");
+					anotacionN("R",num);
+				}
+				marcarPieza(eventPieza);
 				// Con este for eliminamos el evento
 				for (var i = 0; i < optRey.length; i++) {
 					$("#"+optRey[i]).unbind("click");
@@ -363,8 +400,9 @@ function pieza($event, liPieza){
 
 	}
 	function moveDama($event, x, y, xIni, yIni){		
-		
+			
 		var img = $event.currentTarget.currentSrc;
+		var color = img.substr(27,1);
 
 		// Creamos un evento nuevo
 		var eventPieza = $event;
@@ -416,14 +454,26 @@ function pieza($event, liPieza){
 			$("#"+optDama[i]).click(function($event){			
 				piezaSelec[0].parentNode.removeChild(piezaSelec[0]);
 					
-				$($event.currentTarget).html('<li></li>');
+				//$($event.currentTarget).html('<li></li>');
 				// Colocamos la dama en su nueva casilla				
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
+				var num = $(event.currentTarget).attr("id");
+
+				if (color == "B") {
+					$("#"+$event.currentTarget.id).attr("data-pieza","Bdama.png");  				
+					$("#"+$event.currentTarget.id).attr("data-color","Blancas");
+					anotacionB("D",num);
+				}else{
+					$("#"+$event.currentTarget.id).attr("data-pieza","Ndama.png"); 
+					$("#"+$event.currentTarget.id).attr("data-color","Negras");
+					anotacionN("D",num);
+				}
 
 				marcarPieza(eventPieza);
-				var num = $(event.currentTarget).attr("id");
-				anotacion("D",num);
 				for (var x = 0; x < optDama.length; x++) {
+					/*$("#"+$event.currentTarget.id).attr("data-pieza","");  				
+					$("#"+$event.currentTarget.id).attr("data-color","");
+					*/
 					$("#"+optDama[x]).unbind("click");
 				}
 			})
@@ -433,7 +483,7 @@ function pieza($event, liPieza){
 	function moveAlfil($event, x, y, xIni, yIni){		
 
 		var img = $event.currentTarget.currentSrc;
-
+		var color = img.substr(27,1);
 		// Creamos un evento nuevo
 		var eventPieza = $event;
 		var piezaSelec = $($event.currentTarget);
@@ -483,9 +533,19 @@ function pieza($event, liPieza){
 				$($event.currentTarget).html('<li></li>');
 				// Colocamos el alfil en su nueva casilla				
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
-				marcarPieza(eventPieza);
 				var num = $(event.currentTarget).attr("id");
-				anotacion("A",num);
+
+				if (color == "B") {
+					$("#"+$event.currentTarget.id).attr("data-pieza","Balfil.png");  				
+					$("#"+$event.currentTarget.id).attr("data-color","Blancas");
+					anotacionB("A",num);
+				}else{
+					$("#"+$event.currentTarget.id).attr("data-pieza","Nalfil.png"); 
+					$("#"+$event.currentTarget.id).attr("data-color","Negras");
+					anotacionN("A",num);
+				}
+
+				marcarPieza(eventPieza);
 				for (var x = 0; x < optAlfil.length; x++) {
 					$("#"+optAlfil[x]).unbind("click");
 				}
@@ -497,7 +557,7 @@ function pieza($event, liPieza){
 
 		// Recuperamos la imagen
 		var img = $event.currentTarget.currentSrc;
-
+		var color = img.substr(27,1);
 		// Creamos un evento nuevo
 		var eventPieza = $event;
 		var piezaSelec = $($event.currentTarget);
@@ -547,6 +607,19 @@ function pieza($event, liPieza){
 						$($event.currentTarget).html('<li></li>');
 						// Colocamos la dama en su nueva casilla				
 						$($event.currentTarget.firstChild).append(piezaSelec[0]);
+
+						var num = $(event.currentTarget).attr("id");
+
+						if (color == "B") {
+							$("#"+$event.currentTarget.id).attr("data-pieza","Bcaballo.png");  				
+							$("#"+$event.currentTarget.id).attr("data-color","Blancas");
+							anotacionB("C",num);
+						}else{
+							$("#"+$event.currentTarget.id).attr("data-pieza","Ncaballo.png"); 
+							$("#"+$event.currentTarget.id).attr("data-color","Negras");
+							anotacionN("C",num);
+						}
+
 						marcarPieza(eventPieza);
 						var num = $(event.currentTarget).attr("id");
 						anotacion("C",num);
@@ -564,7 +637,7 @@ function pieza($event, liPieza){
 
 		// Recuperamos la imagen
 		var img = $event.currentTarget.currentSrc;
-
+		var color = img.substr(27,1);
 		// Creamos un evento nuevo
 		var eventPieza = $event;
 		var piezaSelec = $($event.currentTarget);
@@ -602,8 +675,18 @@ function pieza($event, liPieza){
 				// Colocamos la dama en su nueva casilla				
 				$($event.currentTarget.firstChild).append(piezaSelec[0]);
 				marcarPieza(eventPieza);
+				
 				var num = $(event.currentTarget).attr("id");
-				anotacion("T",num);
+
+				if (color == "B") {
+					$("#"+$event.currentTarget.id).attr("data-pieza","Btorre.png");  				
+					$("#"+$event.currentTarget.id).attr("data-color","Blancas");
+					anotacionB("T",num);
+				}else{
+					$("#"+$event.currentTarget.id).attr("data-pieza","Ntorre.png"); 
+					$("#"+$event.currentTarget.id).attr("data-color","Negras");
+					anotacionN("T",num);
+				}
 				for (var x = 0; x < optTorre.length; x++) {
 					$("#"+optTorre[x]).unbind("click");
 				}
@@ -611,7 +694,7 @@ function pieza($event, liPieza){
 		}
 	}
 
-	function anotacion(pieza, num){
+	function anotacionB(pieza, num){
 		var tabla = document.getElementById("planilla");
 		var tr = document.createElement("TR");
 		tabla.appendChild(tr);
@@ -624,6 +707,20 @@ function pieza($event, liPieza){
 		var element = document.createTextNode(pieza+letras[num1]+num2);
 		td.appendChild(element);
 		tr.appendChild(td);
+	}
+
+	function anotacionN(pieza, num){
+		var tabla = document.getElementById("planilla");
+		var td = document.createElement("TD");
+		var letras = ["", "a", "b", "c", "d", "e", "f", "g", "h"];
+		var num = $(event.currentTarget).attr("id");
+		var num1 = num.substr(0,1);
+		var num2 = num.substr(1,1);
+
+		var element = document.createTextNode(pieza+letras[num1]+num2);
+		td.appendChild(element);
+		tabla.lastChild.appendChild(td);
+		
 	}
 
 
